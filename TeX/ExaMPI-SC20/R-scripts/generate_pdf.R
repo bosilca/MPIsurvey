c9bc47037f68070b308f450cb740dc8b70a867e6
Emmanuel Jeannot <emmanuel.jeannot@inria.fr>
2020-09-04 16:35:04 +0200

    new version of the plots

--- TeX/ExaMPI-SC20/R-scripts/generate_pdf.R
+++ TeX/ExaMPI-SC20/R-scripts/generate_pdf.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(dplyr)
 library(reshape2)
 
+#change this to your directory the rest should be fine....
 setwd("/Users/ejeannot/recherche/MPIsurvey/TeX")
 
 generate_graph <- function(input_csv = "./dat/Q7-simple.csv", 
@@ -28,9 +29,9 @@ generate_graph <- function(input_csv = "./dat/Q7-simple.csv",
       geom_bar(stat = 'identity', position = 'dodge')+
       theme(legend.position = "none",
         axis.text.y = element_text(size = rel(2)),
-        axis.text.x = element_text(size = rel(2), angle=45, vjust = 1,hjust=1),
+        axis.text.x = element_text(size = rel(3), angle=45, vjust = 1,hjust=1),
         axis.title = element_text(size = rel(2)),
-        strip.text.x = element_text(size = rel(2)),
+        strip.text.x = element_text(size = rel(3)),
         axis.title.x=element_blank(),
         axis.title.y=element_blank(),
         plot.margin=unit(c(0,0,0,lm),"pt"))+
@@ -45,13 +46,13 @@ generate_graph <- function(input_csv = "./dat/Q7-simple.csv",
 generate_graph(input_csv = "./dat/Q7-simple.csv", 
                output_pdf = "./ExaMPI-SC20/R-scripts/Q7.pdf", 
                x_text = c("X","Num-App/Lib", "Lang","OS/R","Tool","Big data","Visulization","AI","Worlflow","Image Proc","Other"),
-               left_margin=30)
+               left_margin=60)
 
 
 generate_graph(input_csv = "./dat/Q17-simple.csv", 
                output_pdf = "./ExaMPI-SC20/R-scripts/Q17.pdf", 
                x_text = c("X", "Collectives" ,"Point-to-point", "Datatypes","with OpenMP","Communicator", "One-sided", "PMPI","Persistent", "Dyn. process", "other"),
-               left_margin=10)
+               left_margin=30)
 
 generate_graph(input_csv = "./dat/Q27-simple.csv", 
                output_pdf = "./ExaMPI-SC20/R-scripts/Q27.pdf", 
@@ -76,13 +77,13 @@ generate_graph(input_csv = "./dat/Q10-simple.csv",
 generate_graph(input_csv = "./dat/Q14-simple.csv", 
                output_pdf = "./ExaMPI-SC20/R-scripts/Q14.pdf", 
                x_text = c("X", "Online docs", "Internet", "MPI standard", "Colleagues", "Books", "I know all", "other"),
-               left_margin=5)
+               left_margin=30)
 
 
 generate_graph(input_csv = "./dat/Q19-simple.csv", 
                output_pdf = "./ExaMPI-SC20/R-scripts/Q19.pdf", 
                x_text = c("X", "No obstacles", "Too many routines", "Complicated", "No appropriate one", "Nobody to ask", "Dislike API", "other"),
-               left_margin=10)
+               left_margin=44)
 
 
 generate_graph(input_csv = "./dat/Q22-simple.csv", 
@@ -93,7 +94,7 @@ generate_graph(input_csv = "./dat/Q22-simple.csv",
 generate_graph(input_csv = "./dat/Q24-simple.csv", 
                output_pdf = "./ExaMPI-SC20/R-scripts/Q24.pdf", 
                x_text = c("X", "No investigation",  "Framework", "PGAS", "DSL", "LL comm", "other"),
-               left_margin=32)
+               left_margin=60)
 
 
 
